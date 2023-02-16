3838c6972ba99109ab9e2b7c6c78590416fe7fea
brhmty <ibrahimmatay@gmail.com>
2023-02-16 17:32:55 +0300

    some improvement

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {
+  AboutPage,
+  CartPage,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  ProductsPage,
+  SingleProductPage,
+} from "./pages/index";
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products:id" element={<SingleProductPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
