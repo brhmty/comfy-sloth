e294873ffebf4bab9095aeed176b57ce2e8930be
brhmty <ibrahimmatay@gmail.com>
2023-02-18 18:41:26 +0300

    filter

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -15,8 +15,18 @@ import { useProductsContext } from "./products_context";
 const initialState = {
   filtered_products: [],
   all_products: [],
-  grid_view: false,
+  grid_view: true,
   sort: "price-lowest",
+  filters: {
+    text: "",
+    company: "all",
+    category: "all",
+    color: "all",
+    min_price: 0,
+    max_price: 0,
+    price: 0,
+    shipping: false,
+  },
 };
 
 const FilterContext = React.createContext();
@@ -30,6 +40,7 @@ export const FilterProvider = ({ children }) => {
   }, [products]);
 
   useEffect(() => {
+    dispatch({ type: FILTER_PRODUCTS });
     dispatch({ type: SORT_PRODUCTS });
   }, [products, state.sort, state.filters]);
 
@@ -40,10 +51,31 @@ export const FilterProvider = ({ children }) => {
     dispatch({ type: SET_LISTVIEW });
   };
   const updateSort = (e) => {
+    // for demonstration
+    // const name = e.target.name
     const value = e.target.value;
     dispatch({ type: UPDATE_SORT, payload: value });
   };
-
+  const updateFilters = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    if (name === "category") {
+      value = e.target.textContent;
+    }
+    if (name === "color") {
+      value = e.target.dataset.color;
+    }
+    if (name === "price") {
+      value = Number(value);
+    }
+    if (name === "shipping") {
+      value = e.target.checked;
+    }
+    dispatch({ type: UPDATE_FILTERS, payload: { name, value } });
+  };
+  const clearFilters = () => {
+    dispatch({ type: CLEAR_FILTERS });
+  };
   return (
     <FilterContext.Provider
       value={{
@@ -51,13 +83,14 @@ export const FilterProvider = ({ children }) => {
         setGridView,
         setListView,
         updateSort,
+        updateFilters,
+        clearFilters,
       }}
     >
       {children}
     </FilterContext.Provider>
   );
 };
-
 export const useFilterContext = () => {
   return useContext(FilterContext);
 };
